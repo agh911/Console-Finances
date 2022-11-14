bcbc10aa5f4d3b180b121c3c147341f410236a38
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-11-14 23:22:01 +0000

    adds further analysis code

--- analysis_file.js
+++ analysis_file.js
@@ -1,12 +1,27 @@
 // Vaiables for storing the needed data
 var months = finances.length;
 var total = 0;
+var net = 0;
+var netChange = 0;
+// Empty array to store each net change
+var netChangeArray = [];
+var netChangeSum = 0;
+var averageChange = 0;
 var analysis = '';
 
 
 // Calculate the total using for loop
 for (var main_index = 0; main_index < finances.length; main_index++) {
-    total += finances[main_index][1];
+    for (var nested_index = 0; nested_index < finances[main_index].length; nested_index++) {
+        // Narrowing to target only number data type
+        if (typeof finances[main_index][nested_index] === 'number') {
+            total += finances[main_index][nested_index];
+            net = finances[main_index][nested_index];
+            netChange = finances[main_index][nested_index] - net;
+            netChangeArray.push(netChange);
+        }
+    }
+    
 }
 
 // Calculate the net total amount of profit/losses in the following manner:
@@ -28,3 +43,5 @@ for (var main_index = 0; main_index < finances.length; main_index++) {
 
 // // Logging analysis in the console
 // console.log(analysis);
+
+console.log(netChangeArray);
