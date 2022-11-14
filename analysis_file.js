065040542b5e3623b981558047b9107b268a85cb
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-11-14 23:51:56 +0000

    adds completed analysis code for average change

--- analysis_file.js
+++ analysis_file.js
@@ -7,6 +7,8 @@ var netChange = 0;
 var netChangeArray = [];
 var netChangeSum = 0;
 var averageChange = 0;
+var greatestIncrease = 0;
+var greatestDecrease = 0;
 var analysis = '';
 
 
@@ -16,22 +18,27 @@ for (var main_index = 0; main_index < finances.length; main_index++) {
         // Narrowing to target only number data type
         if (typeof finances[main_index][nested_index] === 'number') {
             total += finances[main_index][nested_index];
+
+            // Calculate the net total amount of profit/losses in the following manner:
+            // net represented by the number data type in the finances multidimensional array
+            // net change = current period's current price - previous period's closing price
+                // will need an empty array to store values
             netChange = finances[main_index][nested_index] - net;
             net = finances[main_index][nested_index];
             netChangeArray.push(netChange);
         }
     }
-    
 }
 
-// Calculate the net total amount of profit/losses in the following manner:
-    // net represented by the number data type in the array -> create var net w/ initial value of 0
-    // net change = current period's current price - previous period's closing price
-        // will need an empty array to store values
-    // get sum of net change array then divide by nr of months to get average change
-        // format value so that it shows two decimals
+// Get sum of net change array then divide by nr of months to get average change
+    // format value so that it shows two decimals
+for (var change_index = 0; change_index < netChangeArray.length; change_index++) {
+    netChangeSum += netChangeArray[change_index];
+}
 
-        
+averageChange = Math.round((netChangeSum / months) * 100) / 100;
+
+
 // analysis = 
 // 'Financial Analysis' + '\n' +
 // '----------------------------' + '\n' +
@@ -44,4 +51,4 @@ for (var main_index = 0; main_index < finances.length; main_index++) {
 // // Logging analysis in the console
 // console.log(analysis);
 
-console.log(netChangeArray);
+console.log(averageChange);
