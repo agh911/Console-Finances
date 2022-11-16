740bedd485efa570b555f44fd33fcc7d4eb2f266
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-11-16 15:44:06 +0000

    adds changes to analysis

--- analysis_file.js
+++ analysis_file.js
@@ -14,39 +14,33 @@ var analysis = '';
 
 
 // Calculate the total using for loop
-for (var main_index = 0; main_index < finances.length; main_index++) {
-    for (var nested_index = 0; nested_index < finances[main_index].length; nested_index++) {
-        // Narrowing to target only number data type
-        if (typeof finances[main_index][nested_index] === 'number') {
-            total += finances[main_index][nested_index];
-
-            // Calculate the net total amount of profit/losses in the following manner:
-            // net represented by the number data type in the finances multidimensional array
-            // net change = current period's current price - previous period's closing price
-                // will need an empty array to store values
-            netChange = finances[main_index][nested_index] - net;
-            net = finances[main_index][nested_index];
-            netChangeArray.push(netChange);
-
-            // Finding the greatest increase and decrease in the data set
-            if (netChange > greatestIncrease[1]) {
-                greatestIncrease = [finances[main_index][0], finances[main_index][1]];
-            }
-
-            if (netChange < greatestDecrease[1]) {
-                greatestDecrease = [finances[main_index][0], finances[main_index][1]];
-            }
-        }
-    }
-}
-
+for (var main_index = 0; main_index < finances.length; main_index++){
+    total += finances[main_index][1];
+    // Calculate the net total amount of profit/losses in the following manner:
+        // net represented by the number data type in the finances multidimensional array
+        // net change = current period's current price - previous period's closing price
+            // will need an empty array to store values
+    netChange = (finances[main_index][1]) - net;
+    net = finances[main_index][1];
+    netChangeArray.push(netChange);
+    
+    // Finding the greatest increase and decrease in the data set
+    if (netChange > greatestIncrease[1]) {
+                  greatestIncrease = [finances[main_index][0], finances[main_index][1]];
+              }
+  
+              if (netChange < greatestDecrease[1]) {
+                  greatestDecrease = [finances[main_index][0], finances[main_index][1]];
+              }
+  }
+  
 // Get sum of net change array then divide by nr of months to get average change
     // format value so that it shows two decimals
-for (var change_index = 0; change_index < netChangeArray.length; change_index++) {
-    netChangeSum += netChangeArray[change_index];
-}
-
-averageChange = Math.round((netChangeSum / months) * 100) / 100;
+  for (var change_index = 0; change_index < netChangeArray.length; change_index++) {
+      netChangeSum += netChangeArray[change_index];
+  }
+  
+  averageChange = Math.round((netChangeSum / months) * 100) / 100;
 
 
 
